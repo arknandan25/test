43f23ca3d6a5e9dbe21d9c8ac15aa4590c38ed2b
ark <arkchauhan10@gmail.com>
2020-05-17 23:11:33 +0100

    changes made to lanes1.py and jupitar file

--- lanes1.py
+++ lanes1.py
@@ -6,8 +6,8 @@ lanes=np.copy(img)
 grey=cv2.cvtColor(lanes,cv2.COLOR_RGB2GRAY)
 blur=cv2.GaussianBlur(grey,(5,5),0)#to reduce noise and smoothen image
 cv2.imshow('result',blur)
-cv2.waitKey(5000)
+cv2.waitKey(34000)
 cv2.destroyAllWindows()
 #----Gray scale -edge detection-----
 #0(lowest intensity completely black) to 255(highest intensity completely white)
-#values of pixels|
+#values of pixe
